5ec3e57b95cba78dbe35dceab34f753aeb7ac321
Kamil Polikowski <admin@mechadmin.pl>
2017-09-17 13:28:37 +0200

    registration

--- src/AppBundle/Service/Helper/App/Register.php
+++ src/AppBundle/Service/Helper/App/Register.php
@@ -8,15 +8,18 @@ use AppBundle\Entity\User;
 use AppBundle\Entity\UserRole;
 use AppBundle\Entity\Workshop;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class Register
 {
 
     protected $em;
+    protected $encoder;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordEncoderInterface $encoder)
     {
-        $this->em = $entityManager;
+        $this->em       = $entityManager;
+        $this->encoder  = $encoder;
     }
 
     public function createFormData()
@@ -27,81 +30,49 @@ class Register
         ];
     }
 
-    private function createRestData()
-    {
-        return [
-            'settings'      => new Settings(),
-            'parameters'    => new Parameters(),
-            'userRole'      => new UserRole(),
-        ];
-    }
-
     public function processForm($formData)
     {
-        $restData = $this->createRestData();
+        $workshop   = $formData['workshop'];
+        $user       = $formData['user'];
+        
+        $settings   = new Settings();
+        $parameters = new Parameters();
+        $userRole   = new UserRole();
+        
+        $dateTime   = new \DateTime();
 
-        $data = array_merge($formData, $restData);
+        $settings   = new Settings();
+        $parameters = new Parameters();
+        $userRole   = new UserRole();
 
-        $data = $this->setDateTime($data);
+        $settings   ->setCreatedAt($dateTime);
+        $parameters ->setCreatedAt($dateTime);
+        $workshop   ->setCreatedAt($dateTime);
+        $userRole   ->setCreatedAt($dateTime);
+        $user       ->setCreatedAt($dateTime);
 
+        $userRole   ->setUser($user);
+        $userRole   ->setWorkshop($workshop);
+        $userRole   ->setRole(UserRole::ROLE_USER);
 
+        $workshop->setAddress(null);
 
+        $password = $this->encoder->encodePassword($user, $user->getPassword());
+        $user     ->setPassword($password);
+
+        $settings   ->setWorkshop($workshop);
+        $parameters ->setWorkshop($workshop);
+        $workshop   ->setOwnerUser($user);
+        $user       ->setCurrentWorkshop($workshop);
+        $user       ->addWorkshop($workshop);
+
+        $this->em->persist($user);
+        $this->em->persist($workshop);
+        $this->em->persist($parameters);
+        $this->em->persist($settings);
+        $this->em->persist($userRole);
+
+        $this->em->flush();      
 
     }
-
-    private function setDateTime($data)
-    {
-        $dateTime = new \DateTime();
-
-        foreach($data as $entity)
-        {
-            $entity->setCreatedAt($dateTime);
-        }
-
-        return $data;
-    }
-
-
-
-    /*    $em = $this->get('doctrine.orm.default_entity_manager');
-
-    $dateTime = new \DateTime();
-
-    $settings = new Settings();
-    $parameters = new Parameters();
-    $userRole = new UserRole();
-
-    $settings->setCreatedAt($dateTime);
-    $parameters->setCreatedAt($dateTime);
-    $workshop->setCreatedAt($dateTime);
-    $userRole->setCreatedAt($dateTime);
-    $user->setCreatedAt($dateTime);
-
-    $userRole->setUser($user);
-    $userRole->setWorkshop($workshop);
-    $userRole->setRole(UserRole::ROLE_USER);
-
-    $workshop->setAddress(null);
-
-    $encoder = $this->get('security.password_encoder');
-
-    $password = $encoder->encodePassword($user, $user->getPassword());
-
-    $user->setPassword($password);
-
-    $settings->setWorkshop($workshop);
-    $parameters->setWorkshop($workshop);
-    $workshop->setOwnerUser($user);
-    $user->setCurrentWorkshop($workshop);
-    $user->addWorkshop($workshop);
-
-    $em->persist($user);
-    $em->persist($workshop);
-    $em->persist($parameters);
-    $em->persist($settings);
-    $em->persist($userRole);
-
-    $em->flush();
-*/
-
 }
