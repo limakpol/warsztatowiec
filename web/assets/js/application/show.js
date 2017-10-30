755835b08d76620aa507cb67906d15f2a6f0b8c3
Kamil Polikowski <admin@mechadmin.pl>
2017-10-30 04:04:05 +0100

    delivery show, details-table

--- web/assets/js/application/show.js
+++ web/assets/js/application/show.js
@@ -2,6 +2,11 @@ $(document).ready(function()
 {
     $(document).on('click', '.show-box h1 label', function()
     {
-        $(this).parent().parent().find('section').slideToggle();
+        $(this).parent().parent().find('section').slideToggle('slow');
+    });
+
+    $(document).on('click', '.show-box .details-table tr.header', function()
+    {
+        $(this).next('tr.content').find('div.content').slideToggle('slow');
     });
 });
